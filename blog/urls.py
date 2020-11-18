373b8cb112b77d3f0a9d441f2784a22594f082e6
Prawful <prafulla.maharjan@snhu.edu>
2020-11-18 00:33:51 -0500

    Forms update added

--- blog/urls.py
+++ blog/urls.py
@@ -11,8 +11,10 @@ from .views import (
 urlpatterns = [
     path('post/<int:pk>/delete/', # new
         BlogDeleteView.as_view(), name='post_delete'),
-    path('post/new/', BlogCreateView.as_view(), name='post_new'),
-    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('post/new/',
+         BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/',
+         BlogDetailView.as_view(), name='post_detail'),
     path('post/<int:pk>/edit/',
         BlogUpdateView.as_view(), name='post_edit'),
     path('', BlogListView.as_view(), name='home'),
